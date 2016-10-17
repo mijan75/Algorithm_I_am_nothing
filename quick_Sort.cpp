c8585261928551af6d928a8666aab364d794ad86
Mijanur Rahaman <mijanurman750@gmail.com>
2016-10-18 00:04:04 +0600

    Quick Sort Added

---
+++ quick_Sort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void QuickSort(int arr[], int start, int end);
+int partition(int arr[], int start, int end);
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >>arr[i];
+    QuickSort(arr, 0, n-1);
+    for(int i=0; i<n; i++)
+        cout << arr[i]<<" ";
+    cout << endl;
+    return 0;
+}
+
+void QuickSort(int arr[], int start, int end)
+{
+    if(start < end)
+    {
+        int partitionIndex = partition(arr, start, end);
+        QuickSort(arr, start, partitionIndex-1);
+        QuickSort(arr, partitionIndex+1, end);
+    }
+}
+
+int partition(int arr[], int start, int end)
+{
+    int pivot = arr[end];
+    int partitionIndex = start;
+    for(int i=start; i<end; i++)
+    {
+        if(arr[i] <= pivot)
+        {
+            swap(arr[i],arr[partitionIndex]);
+            partitionIndex++;
+        }
+    }
+    swap(arr[partitionIndex],arr[end]);
+    return partitionIndex;
+}
